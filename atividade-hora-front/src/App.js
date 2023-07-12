d032511e4baf526af5a1a2e2effdcae92e2e9e07
Carlos <carlos.oliveira@inteliger.com.br>
2023-07-12 13:40:48 -0300

    configuração de navegação e navegação após login

--- atividade-hora-front/src/App.js
+++ atividade-hora-front/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import SignIn from './components/google-sign-in/index';
+import { Outlet } from 'react-router-dom';
 
 function App() {
 
   return (
     <div className="App">
       <header className="App-header">
-        <SignIn />
+        <h1>TOPO</h1>
+        <Outlet />
+        <p>footer</p>
       </header>
     </div>
   );
