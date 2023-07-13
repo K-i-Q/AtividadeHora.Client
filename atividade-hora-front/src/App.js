f5ca7839aa76f524abb75ccc6ee907952b7d178f
Carlos <carlos.oliveira@inteliger.com.br>
2023-07-13 17:52:46 -0300

    app bar para topo da tela

--- atividade-hora-front/src/App.js
+++ atividade-hora-front/src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 import SignIn from './components/google-sign-in/index';
 import { Outlet } from 'react-router-dom';
+import ResponsiveAppBar from './components/appbar';
 
 function App() {
+  
+  const handleLoginClick = () => {
+    console.log('Clicou no botão de login');
+  };
 
   return (
     <div className="App">
+      <ResponsiveAppBar handleLoginClick={handleLoginClick} />
       <header className="App-header">
-        <h1>TOPO</h1>
         <Outlet />
         <p>footer</p>
       </header>
