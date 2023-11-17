bb5a0e95628463bdc6f11d760f0ae2ed6b07beb0
Carlos <carlos.oliveira@inteliger.com.br>
2023-11-17 11:15:29 -0300

    - nserção de ícones nos botões de login - inserçãode imagem de fundo na tela inicial

--- atividade-hora-front/src/components/appbar/index.js
+++ atividade-hora-front/src/components/appbar/index.js
@@ -1,144 +1,156 @@
-import React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import Menu from '@mui/material/Menu';
-import AdbIcon from '@mui/icons-material/Adb';
-import Tooltip from '@mui/material/Tooltip';
-import Avatar from '@mui/material/Avatar';
-import MenuItem from '@mui/material/MenuItem';
-import { useLocation } from 'react-router-dom';
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import AdbIcon from "@mui/icons-material/Adb";
+import Tooltip from "@mui/material/Tooltip";
+import Avatar from "@mui/material/Avatar";
+import MenuItem from "@mui/material/MenuItem";
+import GoogleIcon from "@mui/icons-material/Google";
+import PersonIcon from "@mui/icons-material/Person";
+import BlurOnIcon from '@mui/icons-material/BlurOn';
 
 
+const settings = ["Profile", "Account", "Dashboard", "Logout"];
 
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+function ResponsiveAppBar({
+  handleLoginClick,
+  isLogado,
+  handleOpenModalEmail,
+  logout,
+}) {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
 
-function ResponsiveAppBar({ handleLoginClick, isLogado, handleOpenModalEmail, logout }) {
-    const [anchorEl, setAnchorEl] = React.useState(null);
-    const [anchorElUser, setAnchorElUser] = React.useState(null);
-    
-    const location = useLocation();
+  const handleOpenMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
 
-    const handleOpenMenu = (event) => {
-        setAnchorEl(event.currentTarget);
-    };
+  const handleCloseMenu = () => {
+    setAnchorEl(null);
+  };
 
-    const handleCloseMenu = () => {
-        setAnchorEl(null);
-    };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
 
-    const handleOpenUserMenu = (event) => {
-        setAnchorElUser(event.currentTarget);
-    };
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
 
-    const handleCloseUserMenu = () => {
-        setAnchorElUser(null);
-    };
-
-    const handleClickSetting = (setting) =>{
-        if(setting === 'Logout'){
-            logout();
-        }
+  const handleClickSetting = (setting) => {
+    if (setting === "Logout") {
+      logout();
     }
+  };
 
-    return (
-        <>
-            <AppBar position="static">
-                <Toolbar>
-                    <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+  return (
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <BlurOnIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+          <Typography
+            variant="h6"
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            Oliveira
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}></Box>
+          <Box sx={{ display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="menu"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenMenu}
+              color="inherit"
+            >
+              <AdbIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorEl)}
+              onClose={handleCloseMenu}
+            ></Menu>
+          </Box>
+          {isLogado ? (
+            <Box sx={{ flexGrow: 0 }}>
+              <Tooltip title="Open settings">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <Avatar alt="Remy Sharp" src={""} />
+                </IconButton>
+              </Tooltip>
+              <Menu
+                sx={{ mt: "45px" }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                {settings.map((setting) => (
+                  <MenuItem key={setting} onClick={handleCloseUserMenu}>
                     <Typography
-                        variant="h6"
-                        component="a"
-                        href="/"
-                        sx={{
-                            mr: 2,
-                            display: { xs: 'none', md: 'flex' },
-                            fontFamily: 'monospace',
-                            fontWeight: 700,
-                            letterSpacing: '.3rem',
-                            color: 'inherit',
-                            textDecoration: 'none',
-                        }}
+                      textAlign="center"
+                      onClick={() => handleClickSetting(setting)}
                     >
-                        Oliveira
+                      {setting}
                     </Typography>
-                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-
-                    </Box>
-                    <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
-                        <IconButton
-                            size="large"
-                            aria-label="menu"
-                            aria-controls="menu-appbar"
-                            aria-haspopup="true"
-                            onClick={handleOpenMenu}
-                            color="inherit"
-                        >
-                            <AdbIcon />
-                        </IconButton>
-                        <Menu
-                            id="menu-appbar"
-                            anchorEl={anchorEl}
-                            anchorOrigin={{
-                                vertical: 'top',
-                                horizontal: 'right',
-                            }}
-                            keepMounted
-                            transformOrigin={{
-                                vertical: 'top',
-                                horizontal: 'right',
-                            }}
-                            open={Boolean(anchorEl)}
-                            onClose={handleCloseMenu}
-                        >
-                        </Menu>
-                    </Box>
-                    {isLogado ?
-                        <Box sx={{ flexGrow: 0 }}>
-                            <Tooltip title="Open settings">
-                                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                                    <Avatar alt="Remy Sharp" src={''} />
-                                </IconButton>
-                            </Tooltip>
-                            <Menu
-                                sx={{ mt: '45px' }}
-                                id="menu-appbar"
-                                anchorEl={anchorElUser}
-                                anchorOrigin={{
-                                    vertical: 'top',
-                                    horizontal: 'right',
-                                }}
-                                keepMounted
-                                transformOrigin={{
-                                    vertical: 'top',
-                                    horizontal: 'right',
-                                }}
-                                open={Boolean(anchorElUser)}
-                                onClose={handleCloseUserMenu}
-                            >
-                                {settings.map((setting) => (
-                                    <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                                        <Typography textAlign="center" onClick={() => handleClickSetting(setting)}>{setting}</Typography>
-                                    </MenuItem>
-                                ))}
-                            </Menu>
-                        </Box>
-                        : <>
-                            <Button color="inherit" onClick={() => handleLoginClick(true,null,null,null)}>
-                                Login com Google
-                            </Button>
-                            <Button color="inherit" onClick={() => handleOpenModalEmail()}>
-                                Login com email e senha
-                            </Button>
-                        </>
-                    }
-                </Toolbar>
-            </AppBar>
-        </>
-    );
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+          ) : (
+            <>
+              <Button
+                variant="contained"
+                color="success"
+                onClick={() => handleLoginClick(true, null, null, null)}
+                endIcon={<GoogleIcon />}
+              >
+                Entrar com Google
+              </Button>
+              <Button color="inherit" onClick={() => handleOpenModalEmail()} endIcon={<PersonIcon />}>
+                Entrar com usuário e senha
+              </Button>
+            </>
+          )}
+        </Toolbar>
+      </AppBar>
+    </>
+  );
 }
 
 export default ResponsiveAppBar;
